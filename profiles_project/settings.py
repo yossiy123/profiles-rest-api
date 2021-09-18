74e3e6c21e39c74559e026ace3f6d77d0515df44
Yossi <yossiy11@gmail.com>
2021-09-18 20:54:02 +0300

    DB

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 # Set this for make Django use our custom User
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
